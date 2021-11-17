e5e8d2e3610242524ab78d8111625ad944579f6a
joelruiz2 <joelruiz2@gmail.com>
2021-11-16 22:36:23 -0600

    cleaning up the create game inputs

--- src/components/dev/DevManageGameFields.js
+++ src/components/dev/DevManageGameFields.js
@@ -67,101 +67,120 @@ class DevManageGameFields extends Component {
     render() {
         let hasError = (this.props.devgameerror && this.props.devgameerror.length > 0);
         return (
-            <div id="devmanagegamefields" className="inputform">
+            <div id="devmanagegamefields" className="form">
                 <h3>Finish up, so you can publish.</h3>
-
-                <DevImageUpload
-                    imgstore='devgameimages'
-                    uploadFunc={uploadGameImages} />
-
-                {hasError && this.displayError()}
-
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Game Name"
-                    maxLength="60"
-                    value={this.props.devgame.name || ''}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-                <input
-                    type="text"
-                    name="game_slug"
-                    placeholder="Slug Name (lower a-z and - only)"
-                    maxLength="32"
-                    value={this.props.devgame.game_slug || ''}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-                <label htmlFor="version">Version</label>
-                <input
-                    type="text"
-                    disabled
-                    name="version"
-                    id="version"
-                    placeholder="Version"
-                    maxLength="12"
-                    value={this.props.devgame.version || '1'} />
-                <br />
-                <label htmlFor="maxplayers">Max players</label>
-                <input
-                    type="number"
-                    name="maxplayers"
-                    id="maxplayers"
-                    placeholder="Max Players"
-                    min="1"
-                    max="100"
-                    value={this.props.devgame.maxplayers || '2'}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-
-                <label htmlFor="minplayers">Min players</label>
-                <input
-                    type="number"
-                    name="minplayers"
-                    id="minplayers"
-                    placeholder="Min Players"
-                    min="1"
-                    max="100"
-                    value={this.props.devgame.minplayers || '2'}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-                <label htmlFor="teams">Team names</label>
-                <input
-                    type="text"
-                    name="teams"
-                    id="teams"
-                    placeholder="i.e. Red, Blue"
-                    maxLength="80"
-                    value={this.props.devgame.teams || ''}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-                <input
-                    type="text"
-                    name="shortdesc"
-                    placeholder="Short Description"
-                    maxLength="80"
-                    value={this.props.devgame.shortdesc || ''}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-                <textarea
-                    type="text"
-                    name="longdesc"
-                    placeholder="Long Description"
-                    maxLength="1200"
-                    value={this.props.devgame.longdesc || ''}
-                    onChange={this.inputChange.bind(this)}></textarea>
-                <br />
-                <input
-                    type="text"
-                    name="git"
-                    placeholder="Git URL for this project"
-                    maxLength="255"
-                    value={this.props.devgame.git || ''}
-                    onChange={this.inputChange.bind(this)} />
-                <br />
-
-
-                <button onClick={this.onSubmit.bind(this)}>Save</button>
+                <div className="form-row">
+                    <DevImageUpload
+                        imgstore='devgameimages'
+                        uploadFunc={uploadGameImages} />
+                </div>
+                <div className="form-row">
+                    {hasError && this.displayError()}
+                </div>
+                <div className="form-row">
+                    <label htmlFor="name">Game Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Game Name"
+                        maxLength="60"
+                        value={this.props.devgame.name || ''}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="game_slug">Game Slug</label>
+                    <input
+                        type="text"
+                        name="game_slug"
+                        id="game_slug"
+                        placeholder="Slug Name (lower a-z and - only)"
+                        maxLength="32"
+                        value={this.props.devgame.game_slug || ''}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="version">Version</label>
+                    <input
+                        type="text"
+                        disabled
+                        name="version"
+                        id="version"
+                        placeholder="Version"
+                        maxLength="12"
+                        value={this.props.devgame.version || '1'} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="maxplayers">Max players</label>
+                    <input
+                        type="number"
+                        name="maxplayers"
+                        id="maxplayers"
+                        placeholder="Max Players"
+                        min="1"
+                        max="100"
+                        value={this.props.devgame.maxplayers || '2'}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="minplayers">Min players</label>
+                    <input
+                        type="number"
+                        name="minplayers"
+                        id="minplayers"
+                        placeholder="Min Players"
+                        min="1"
+                        max="100"
+                        value={this.props.devgame.minplayers || '2'}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="teams">Team names</label>
+                    <input
+                        type="text"
+                        name="teams"
+                        id="teams"
+                        placeholder="i.e. Red, Blue"
+                        maxLength="80"
+                        value={this.props.devgame.teams || ''}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="shortdesc">Short Description</label>
+                    <input
+                        type="text"
+                        name="shortdesc"
+                        id="shortdesc"
+                        placeholder="Short Description"
+                        maxLength="80"
+                        value={this.props.devgame.shortdesc || ''}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <label htmlFor="longdesc">Long Description</label>
+                    <textarea
+                        type="text"
+                        name="longdesc"
+                        id="longdesc"
+                        placeholder="Long Description"
+                        maxLength="1200"
+                        value={this.props.devgame.longdesc || ''}
+                        onChange={this.inputChange.bind(this)}></textarea>
+                </div>
+                <div className="form-row">
+                    <label htmlFor="git">Git URL for issues</label>
+                    <input
+                        type="text"
+                        name="git"
+                        id="git"
+                        placeholder="https://github.com/myname/myrepo"
+                        maxLength="255"
+                        value={this.props.devgame.git || ''}
+                        onChange={this.inputChange.bind(this)} />
+                </div>
+                <div className="form-row">
+                    <button className="submit" onClick={this.onSubmit.bind(this)}>Save</button>
+                </div>
                 {
                     hasError && (
                         <div>
