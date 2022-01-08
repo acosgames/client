5a21bccfe9f5fa54e32121614f98c5e5e7afc00c
Joel <joelruiz2@gmail.com>
2022-01-08 01:09:17 -0600

    fixing footer for mobile

--- src/components/AcosFooter.js
+++ src/components/AcosFooter.js
@@ -6,8 +6,8 @@ import { FaGithub, FaTwitter } from '@react-icons';
 function AcosFooter() {
     return (
         <VStack pb={'2rem'} w="100%" justifyContent={'center'}>
-            <Divider mt={10} mb={8} />
-            <HStack spacing="2rem">
+            <Divider mt={[3, 3, 10]} mb={[3, 3, 8]} />
+            <HStack spacing="2rem" display={['none', 'none', 'flex']}>
                 <Text as="span" fontWeight="100" fontSize="xs">Copyright © 2022 Acos</Text>
                 <Text fontSize="xs"><Link to="/">Games</Link></Text>
                 <Text fontSize="xs"><Link to="/dev">Dev Zone</Link></Text>
@@ -17,6 +17,20 @@ function AcosFooter() {
                 <Text fontSize="md"><ChLink href="https://github.com/acosgames"><Icon as={FaGithub} /></ChLink></Text>
                 <Text fontSize="md"><ChLink href="https://twitter.com/acosgames"><Icon as={FaTwitter} /></ChLink></Text>
             </HStack>
+
+            <VStack display={['flex', 'flex', 'none']}>
+                <HStack spacing="1.4rem" >
+                    <Text fontSize="xs"><Link to="/">Games</Link></Text>
+                    <Text fontSize="xs"><Link to="/dev">Dev Zone</Link></Text>
+                    <Text fontSize="xs"><ChLink isExternal href="https://docs.acos.games">Docs</ChLink></Text>
+                    <Text fontSize="xs"><Link to="/privacy">Privacy</Link></Text>
+                    <Text fontSize="xs"><Link to="/terms">Terms</Link></Text>
+                    <Text fontSize="md"><ChLink href="https://github.com/acosgames"><Icon as={FaGithub} /></ChLink></Text>
+                    <Text fontSize="md"><ChLink href="https://twitter.com/acosgames"><Icon as={FaTwitter} /></ChLink></Text>
+                </HStack>
+                <Text as="span" fontWeight="100" fontSize="xs">Copyright © 2022 Acos</Text>
+            </VStack>
+
 
         </VStack>
     )
