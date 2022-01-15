fb3c503dc63bb045f76ed246cb80d9e6bd4be44a
Joel <joelruiz2@gmail.com>
2022-01-15 03:15:28 -0600

    added webpush notifications, only MacOS and iOS not supported yet

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -25,7 +25,7 @@ const isLocalhost = Boolean(
 export function register(config) {
     if ('serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
-        const publicUrl = new URL(CONFIG.https.api, window.location.href);
+        const publicUrl = new URL(CONFIG.https.website, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
